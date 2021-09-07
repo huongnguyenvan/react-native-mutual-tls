6c3b7d324f52b49bc1766b15eaf179f60af135e0
Huong Nguyen <huong.nguyenvan@morsoftware.com>
2021-09-07 15:45:31 +0700

    Support config for android

---
+++ android/src/main/java/com/reactlibrary/CustomClientFactory.java
@@ -0,0 +1,84 @@
+package com.reactlibrary;
+
+import android.annotation.SuppressLint;
+import android.util.Base64;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.network.OkHttpClientFactory;
+import com.facebook.react.modules.network.ReactCookieJarContainer;
+
+import okhttp3.OkHttpClient;
+
+public class CustomClientFactory implements OkHttpClientFactory {
+    public static String certificateFileP12;
+    public static String certificatePassword;
+
+    @Override
+    public OkHttpClient createNewNetworkModuleClient() {
+        String TAG = "OkHttpClientFactory";
+
+        try {
+            byte[] decbytes = Base64.decode(certificateFileP12,Base64.DEFAULT);
+            InputStream stream = new ByteArrayInputStream(decbytes);
+
+            KeyStore keyStore = KeyStore.getInstance("PKCS12");
+            keyStore.load(stream, certificatePassword.toCharArray());
+
+            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("X509");
+            keyManagerFactory.init(keyStore, certificatePassword.toCharArray());
+
+            X509TrustManager trustManager = new X509TrustManager() {
+                @SuppressLint("TrustAllX509TrustManager")
+                @Override
+                public void checkClientTrusted(X509Certificate[] chain, String authType) {
+                }
+
+                @SuppressLint("TrustAllX509TrustManager")
+                @Override
+                public void checkServerTrusted(X509Certificate[] chain, String authType) {
+                }
+
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }
+            };
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(keyManagerFactory.getKeyManagers(), new TrustManager[]{ trustManager }, new SecureRandom());
+            return new OkHttpClient.Builder()
+                    .sslSocketFactory(sslContext.getSocketFactory(), trustManager)
+                    .hostnameVerifier(new HostnameVerifier() {
+                        @Override
+                        public boolean verify(String hostname, SSLSession session) {
+                            return true;
+                        }
+                    })
+                    .cookieJar(new ReactCookieJarContainer()).build();
+
+        } catch (
+                Exception e) {
+            Log.e(TAG, e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+}
--- android/src/main/java/com/reactlibrary/MutualTLSModule.java
+++ android/src/main/java/com/reactlibrary/MutualTLSModule.java
@@ -4,11 +4,13 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.modules.network.OkHttpClientProvider;
+
 
 public class MutualTLSModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
-
     public MutualTLSModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
@@ -19,9 +21,20 @@ public class MutualTLSModule extends ReactContextBaseJavaModule {
         return "MutualTLS";
     }
 
+
+    @ReactMethod
+    public void configure(ReadableMap readableMap) {
+        CustomClientFactory.certificateFileP12 = readableMap.getString("certificateFileP12");
+        CustomClientFactory.certificatePassword = readableMap.getString("certificatePassword");
+        OkHttpClientProvider.setOkHttpClientFactory(new CustomClientFactory());
+    }
+
     @ReactMethod
     public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
         // TODO: Implement some actually useful functionality
         callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
     }
+
+
+
 }
